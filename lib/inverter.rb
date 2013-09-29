16ca7e4211e30c0a38e596aae31339fd6caff054
trieck <trieck@gmail.com>
2013-09-29 18:06:34 -0400

    incremental

--- lib/inverter.rb
+++ lib/inverter.rb
@@ -1,11 +1,42 @@
+require 'inverter_recs'
+
 class Inverter
 
-  def initialize
+  MAX_COUNT = 100000 # maximum number of index records
 
+  attr_reader :count
+
+  def initialize
+    @records = InverterRecs.new # hash table of records
+    @count = 0 # number of records in table
   end
 
   def is_full?
-    return false
+    @count >= MAX_COUNT
+  end
+
+  def compact
+    @records.compact
+  end
+
+  def clear
+    @records.clear
+    @count = 0
+  end
+
+  def write(io)
+    compact
+    sort
+
+    @count.times do |i|
+      term = @records.term(i)
+      anchors = @records.anchors(i)
+
+      # write term
+      # write anchors.position
+
+    end
+
   end
 
   def insert(term, anchor)
